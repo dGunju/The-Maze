3e8d4825ac65b8728582857bc508ef101eec7898
dGunju <jonathanadegunju@gmail.com>
2023-05-31 13:50:37 +0000

    main.c and edited heaser file

--- headers/header.h
+++ headers/header.h
@@ -127,4 +127,32 @@ void renderRays(void);
 void horzIntersection(float rayAngle);
 void vertIntersection(float rayAngle);
 
+/* Functions-variables-structs for textures */
+
+/**
+ * struct texture_s - struct for the textures
+ * @width: texture width
+ * @height: texture height
+ * @texture_buffer: pointer to texture buffer
+ * @upngTexture: pointer to upng buffer
+ *
+ */
+
+typedef struct texture_s
+{
+	int width;
+	int height;
+	color_t *texture_buffer;
+	upng_t *upngTexture;
+} texture_t;
+
+texture_t wallTextures[NUM_TEXTURES];
+
+void WallTexturesready(void);
+void freeWallTextures(void);
+
+/* Functions-variables-structs for walls */
+
+void renderWall(void);
+
 #endif /*HEADER_H*/
